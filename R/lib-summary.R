a24a18ffe82695dad171621ce905e95d72ab6f81
Paul Landahl <paul.landahl@thrivent.com>
2023-09-18 12:13:12 -0500

    changed function name Added documentation

--- R/lib-summary.R
+++ R/lib-summary.R
@@ -1,6 +1,19 @@
-library_summary <- function() {
+
+#' R Library Summary
+#'
+#' Provides a brief summary of the pakcage libraries
+#' on your machine
+#'
+#' @return A `data.frame` containing a count of packages in eachof the user's
+#' libraries.
+#' @export
+#'
+#' @examples
+#' lib_summary()
+lib_summary <- function() {
   pkgs <- utils::installed.packages()
   pkg_df <- as.data.frame(table(pkgs[, 'LibPath']), stringsAsFactors = FALSE)
   names(pkg_df) <- c("Library", "n_packages")
   return(pkg_df)
 }
+
